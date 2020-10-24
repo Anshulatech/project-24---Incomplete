97a1570879132ab8efe8762c98e6703959183ac5
Anshulatech <73025709+Anshulatech@users.noreply.github.com>
2020-10-24 13:13:57 -0400

    Add files via upload
    this is incomplete, I am confused regarding the contents of the display part in a class

---
+++ paper.js
@@ -0,0 +1,11 @@
+class paper{
+    constructor(x,y,radius){
+        var options={isStatic:false,restitution:0.3,friction:0.5,density:1.2}
+        this.paper = Bodies.circle(x,y,radius, options);
+        World.add(world, this.paper);
+    }
+
+    display(){
+        
+    }
+}
---
+++ trash.js
@@ -0,0 +1,9 @@
+class trash{
+    constructor(){
+
+    }
+
+    display(){
+        
+    }
+}
